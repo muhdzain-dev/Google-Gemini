663ac2ccc52cfc0ade204fa199c9dc4f19975482
Muhammad Zain <muhd.zain.dev@gmail.com>
2024-10-29 12:20:17 +0400

    Update gimini.js

--- google-gemini/src/config/gimini.js
+++ google-gemini/src/config/gimini.js
@@ -4,7 +4,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = "AIzaSyD9JloyWT-RXqQyh_eCiomm6w4uq_U3byw";
+const apiKey = "";
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
